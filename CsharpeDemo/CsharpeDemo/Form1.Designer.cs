7f55d84aefac45f9047c2b5409623ed3fcc75b33
Sieng Sotheara <sieng.sotheara.7@gmail.com>
2016-06-25 14:28:49 +0700

    asd

--- CsharpeDemo/CsharpeDemo/Form1.Designer.cs
+++ CsharpeDemo/CsharpeDemo/Form1.Designer.cs
@@ -39,12 +39,13 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(479, 425);
+            this.ClientSize = new System.Drawing.Size(587, 425);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
--- CsharpeDemo/CsharpeDemo/Form1.cs
+++ CsharpeDemo/CsharpeDemo/Form1.cs
@@ -14,5 +14,10 @@ namespace CsharpeDemo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
